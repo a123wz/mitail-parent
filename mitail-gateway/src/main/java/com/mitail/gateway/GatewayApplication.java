80b28b1e9e19020e91ef12f197c35decd4269cf1
liu <272717803@qq.com>
2022-04-13 21:50:30 +0800

    修改版本号

--- mitail-gateway/src/main/java/com/mitail/gateway/GatewayApplication.java
+++ mitail-gateway/src/main/java/com/mitail/gateway/GatewayApplication.java
@@ -11,44 +11,4 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-
-    /*@Bean
-    public WebSocketClient getWebSocketClient(){
-//        return new JettyWebSocketClient();
-//        return new ReactorNettyWebSocketClient();
-//        return new UndertowWebSocketClient();
-//        return new TomcatWebSocketClient();
-//        return null;
-        *//*return new WebSocketClient(){
-            @Override
-            public Mono<Void> execute(URI uri, WebSocketHandler webSocketHandler) {
-                log.info("1111111111111");
-                return null;
-            }
-
-            @Override
-            public Mono<Void> execute(URI uri, HttpHeaders httpHeaders, WebSocketHandler webSocketHandler) {
-                log.info("222222222");
-                return null;
-            }
-        };*//*
-    }*/
-
-    /*@Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-        String httpUri = "http://httpbin.org:80";
-        return builder.routes()
-            .route(p -> p
-                .path("/get")
-                .filters(f -> f.addRequestHeader("Hello", "World"))
-                .uri(httpUri))
-            .route(p -> p
-                .host("*.hystrix.com")
-                .filters(f -> f
-                    .hystrix(config -> config
-                        .setName("mycmd")
-                        .setFallbackUri("forward:/fallback")))
-                .uri(httpUri))
-            .build();
-    }*/
 }
